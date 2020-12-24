55cde3d7ed30bb9622f419c97f079eed7eb8bf6d
bitzhengqj@163.com <qinjian.zheng@student.unsw.edu.au>
2020-12-24 14:55:32 +0800

    add: linux kernel double linked list implementation

--- ds/line/list/linklist/double/kernel/list.h
+++ ds/line/list/linklist/double/kernel/list.h
@@ -1,15 +1,60 @@
 #ifndef LINUX_LIST_H__
 #define LINUX_LIST_H__
 
-#define LIST_HEAD_INIT(name) {&(name), &(name)}
-#define LIST_HEAD(head) \
-    struct list_head name = LIST_HEAD_INIT(name)
-
 struct list_head
 {
-    list->prev = list;
-    list->next = list;
+    struct list_head *prev;
+    struct list_head *next;
 };
 
+#define LIST_HEAD_INIT(name) {&(name), &(name)}
+
+#define LIST_HEAD(name) \
+    struct list_head name = LIST_HEAD_INIT(name)
+
+#define list_for_each(pos, head) \
+	for (pos = (head)->next; pos != (head); pos = pos->next)
+
+#define offsetof(TYPE, MEMBER)	((size_t)&((TYPE *)0)->MEMBER)
+
+#define __compiletime_assert(condition, msg, prefix, suffix) do { } while (0)
+
+#define _compiletime_assert(condition, msg, prefix, suffix) \
+	__compiletime_assert(condition, msg, prefix, suffix)
+
+#define compiletime_assert(condition, msg) \
+	_compiletime_assert(condition, msg, __compiletime_assert_, __COUNTER__)
+
+#define BUILD_BUG_ON_MSG(cond, msg) compiletime_assert(!(cond), msg)
+
+#define __same_type(a, b) __builtin_types_compatible_p(typeof(a), typeof(b))
+
+#define container_of(ptr, type, member) ({				\
+	void *__mptr = (void *)(ptr);					\
+	BUILD_BUG_ON_MSG(!__same_type(*(ptr), ((type *)0)->member) &&	\
+			 !__same_type(*(ptr), void),			\
+			 "pointer type mismatch in container_of()");	\
+	((type *)(__mptr - offsetof(type, member))); })
+
+
+#define list_entry(ptr, type, member) \
+	container_of(ptr, type, member)
+
+
+
+static inline void __list_add(struct list_head *new,
+			      struct list_head *prev,
+			      struct list_head *next)
+{
+	next->prev = new;
+	new->next = next;
+	new->prev = prev;
+	prev->next = new;
+}
+
+static inline void list_add(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head, head->next);
+}
 
 #endif
--- ds/line/list/linklist/double/kernel/main.c
+++ ds/line/list/linklist/double/kernel/main.c
@@ -13,10 +13,20 @@ struct score_st
 
 };
 
+static void print_score(struct score_st *d)
+{
+    printf("%d %s %d %d\n", d->id, d->name, d->math, d->chinese);
+}
+
 int main(int argc, char const *argv[])
 {
     struct score_st *datap;
+    struct list_head *cur;
+
     LIST_HEAD(head);
+    
+    // struct list_head head = LIST_HEAD_INIT(head);
+    // struct list_head head = {&(head), &(head)};
     for(int i = 0; i < 7; i++)
     {
         datap = malloc(sizeof(*datap));
@@ -26,7 +36,27 @@ int main(int argc, char const *argv[])
         snprintf(datap->name, NAMESIZE, "std%d", i);
         datap->math = rand() % 100;
         datap->chinese = rand() % 100;
+
+        list_add(&datap->node, &head);
     }
+
+    list_for_each(cur, &head)
+    {
+        datap = list_entry(cur, struct score_st, node);
+        print_score(datap);
+    }
+
+    list_for_each(cur, &head)
+    {
+        datap = list_entry(cur, struct score_st, node);
+        if(datap->id == 15)
+            break;
+    }
+    if(cur == &head)
+        printf("Not found!\n");
+    else
+        print_score(datap);
+
 
     return 0;
 }
